b4ca6dd6cf7ad572ee91e4e4a1c9d2646c554db2
gabi <gabrielleonardomendoza19@gmail.com>
2021-09-14 20:51:38 -0300

    Recorriendo el dom

--- dom.js
+++ dom.js
@@ -185,5 +185,24 @@ $whatIsDOM.outerHTML = text;
 */
 
 ///////////////////---------------- DOM Traversing: Recorriendo el DOM -----------------//////////////////
+/*
+const $cards = document.querySelector(".cards");
+
+console.log($cards);
+console.log($cards.children);
+console.log($cards.children[2]);
+console.log($cards.parentElement);
+console.log($cards.firstElementChild);
+console.log($cards.lastElementChild);
+console.log($cards.previousElementSibling);
+console.log($cards.nextElementSibling);
+console.log($cards.closest('div'));
+console.log($cards.closest('body'));
+console.log($cards.children[1].closest('section'));
+*/
+
+//////////////////--------------- DOM: Creando Elementos y Fragmentos ---------------///////////////////
+
+
 
 
